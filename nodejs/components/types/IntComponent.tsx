cb7c3b9dda225f8c341e3f1b5438c1f4df5284d9
sshcrack <hendrik.lind777@gmail.com>
2022-02-18 21:22:48 +0100

    moved files around

--- nodejs/components/types/IntComponent.tsx
+++
@@ -1,6 +0,0 @@
-import { VarProps } from '../interface';
-import GeneralNumber from './GeneralNumber';
-
-export default function IntComponent({ curr, onChange, variable }: VarProps<number>) {
-    return <GeneralNumber curr={curr} onChange={onChange} variable={variable} step={1} float={false} />
-}
