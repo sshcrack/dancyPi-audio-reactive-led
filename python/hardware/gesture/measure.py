500c445650ca792b21e15e48c32460528fbd9b75
sshcrack <34072808+sshcrack@users.noreply.github.com>
2022-02-19 15:23:55 +0100

    Update measure.py

--- python/hardware/gesture/measure.py
+++ python/hardware/gesture/measure.py
@@ -1,44 +1,37 @@
 import hardware.gesture.grove_gesture_sensor as grove_gesture_sensor
-import RPi.GPIO as GPIO
 from threading import Thread
 import time
-from config import STATUS_LED_PIN
+from config import STATUS_LED_PIN, GESTURE_SENSOR_ENABLED
 from httpserver.currVars import getConfig, setConfig
-
 shouldRun = True
 
-g=grove_gesture_sensor.gesture()
+if GESTURE_SENSOR_ENABLED:
+        g=grove_gesture_sensor.gesture()
 
-GPIO.setmode(GPIO.BCM)
 
 def hasGesture():
         gest=g.return_gesture()
         return gest != 0
 
-def ledUpdate(enabled: bool):
-    GPIO.output(STATUS_LED_PIN, enabled)
-
 def measureThread():
     print("Initializing gesture...")
+    if not GESTURE_SENSOR_ENABLED:
+        return
 
     g.init()
-    GPIO.setup(STATUS_LED_PIN, GPIO.OUT)
 
     global shouldRun
     print(f"Should run {shouldRun}")
 
-    ledUpdate(isEnabled())
     while shouldRun:
         if hasGesture():
             enabled = not isEnabled()
 
             setConfig("enabled", enabled)
             print(f"Setting enabled to {enabled}")
-            ledUpdate(enabled)
 
         time.sleep(.1)
 
-    GPIO.cleanup()
     print("Exiting thread...")
 
 def isEnabled():
@@ -48,5 +41,5 @@ def stop():
     global shouldRun
     shouldRun = False
 
-thread1 = Thread(target=measureThread, daemon=False)
+thread1 = Thread(target=measureThread, daemon=True)
 thread1.start()
