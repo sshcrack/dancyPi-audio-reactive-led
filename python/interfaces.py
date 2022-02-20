8a1330e35d12f951641ddac38e30458cb8906aff
sshcrack <34072808+sshcrack@users.noreply.github.com>
2022-02-20 19:57:11 +0100

    Update interfaces.py

--- python/interfaces.py
+++ python/interfaces.py
@@ -1,5 +1,10 @@
 import subprocess
 
+
+rc = subprocess.call(['which', 'ifconfig'])
+if rc != 0:
+    print 'Make sure to be running as root'
+
 def getIPs():
     out = subprocess.run([ "sudo", "ifconfig", "-a"], capture_output=True).stdout.decode("utf-8")
     newLines = filter(lambda e: "inet " in e, out.split("\n"))
