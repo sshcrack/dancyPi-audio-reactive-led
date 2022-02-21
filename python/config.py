95965f47d5f11ecee452a13161d958cf4406a601
sshcrack <34072808+sshcrack@users.noreply.github.com>
2022-02-21 22:15:10 +0100

    Update config.py

--- python/config.py
+++ python/config.py
@@ -32,7 +32,7 @@ N_PIXELS = 150
 STATUS_LED_PIN = 4
 """Pin of the status led to use"""
 
-GESTURE_SENSOR_ENABLED = True
+GESTURE_SENSOR_ENABLED = False
 """Weither a Groove Gesture Sensor is connected to the pi"""
 
 GAMMA_TABLE_PATH = os.path.join(os.path.dirname(__file__), 'modes', 'visualization', 'gamma_table.npy')
