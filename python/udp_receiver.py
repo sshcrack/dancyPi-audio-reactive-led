abb2585b02730d580c1d2af431f00837125ccf51
sshcrack <hendrik.lind777@gmail.com>
2022-06-01 20:32:52 +0200

    Added threading

--- python/udp_receiver.py
+++ python/udp_receiver.py
@@ -1,11 +1,15 @@
+#!/bin/python3
 import socket
+from threading import Thread
 import board
 from neopixel import NeoPixel
 
-PIXELS = 16
 
+PIXELS = 150
+
+#ip      = "127.0.0.1"
 ip = "0.0.0.0"
-port = 6189
+port    = 6189
 
 listeningAddress = (ip, port)
 
@@ -14,13 +18,29 @@ s.bind(listeningAddress)
 
 BUFF_LENGTH = 1024
 
-strip = NeoPixel(board.D18, PIXELS)
+strip = NeoPixel(board.D18, PIXELS, auto_write=False)
 
-while True:
-    v, _ = s.recvfrom(BUFF_LENGTH)
-    for i in range(0, len(v), 4):
-        led = v[i]
-        r = v[i + 1]
-        g = v[i + 2]
-        b = v[i + 3]
-        strip[led] = (r, g, b)
+
+def show():
+    strip.show()
+
+try:
+    while(True):
+        v, _ = s.recvfrom(BUFF_LENGTH)
+        for i in range(0, len(v), 4):
+            led = v[i]
+            r = v[i +1]
+            g = v[i +2]
+            b = v[i +3]
+
+            if led >= PIXELS:
+                continue
+            strip[led] = (r, g, b)
+        th = Thread(target=show)
+        th.start()
+except KeyboardInterrupt:
+    pass
+finally:
+    for i in range(PIXELS):
+        strip[i] = (0, 0, 0)
+    strip.show()
